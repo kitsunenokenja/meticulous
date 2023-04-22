290cb9f076630a24cba91833f856de52bc315188
Rob Levitsky <kitsunenokenja@users.noreply.github.com>
2023-04-21 21:36:01 -0400

    Switch to the new API for the weather widget

--- weather.lua
+++ weather.lua
@@ -10,23 +10,36 @@
 -- Copyright (c) 2017
 ]]--
 
+local value = nil
+
 return function(args)
    if args["lat"] == nil or args["lon"] == nil then
       return nil
    end
-   local url = "https://api.met.no/weatherapi/locationforecast/1.9"
 
-   local f = io.popen(
-      "curl -s '" .. url .. "/?lat=" .. args["lat"] ..
-      "&lon=" .. args["lon"] .. "'"
-   )
-   for line in f:lines() do
-      if string.match(line, "temperature") ~= nil then
-         f:close()
-         return tonumber(string.match(line, "value=\"(.+)\""))
+   local awful = require("awful")
+   local url = "https://api.met.no/weatherapi/locationforecast/2.0/classic"
+   local flag = false
+
+   local cmd = "curl -m 8 -s '" .. url .. "/?lat=" .. args["lat"] ..
+      "&lon=" .. args["lon"] .. "'" ..  " -H 'User-Agent: Firefox'"
+   awful.spawn.with_line_callback(cmd, {
+      stdout = function(line)
+         if flag then
+            return
+         end
+         if string.match(line, "temperature") ~= nil then
+            value = tonumber(string.match(line, "value=\"(.+)\""))
+            flag = true
+         end
       end
+   })
+
+   if value == nil then
+      return "N/A"
+   else
+      return value
    end
-   return "N/A"
 end
 
 -- vim: set ts=3 sw=3 et :
